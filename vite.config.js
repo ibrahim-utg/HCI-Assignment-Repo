dbca292de78491cb623c4008cc1204df227c9e9b
ibrahimceesay <ibraceesay981@gmail.com>
2026-06-26 16:48:15 +0000

    feat: update package.json and main.jsx for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/HCI-Assignment-Repo/",
   plugins: [react(), tailwindcss()],
 });
